0d222eb8419c1380733a991d33a37226a3b0132c
yingyuekk <1308056152@qq.com>
2020-03-05 13:17:55 +0800

    SpringBoot 使用WebSocket打造在线聊天室（基于注解）

---
+++ src/main/java/cn/xq/demo/chat/WebSocketConfig.java
@@ -0,0 +1,26 @@
+package cn.xq.demo.chat;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+/**
+ * 功能描述
+ *
+ * @author xieqiong
+ * @date 2020/3/5
+ */
+@Configuration
+public class WebSocketConfig
+{
+    /**
+     * 用于扫描和注册所有携带ServerEndPoint注解的实例。
+     * <p>
+     * PS:若部署到外部容器 则无需提供此类。
+     */
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+
+        return new ServerEndpointExporter();
+    }
+}
